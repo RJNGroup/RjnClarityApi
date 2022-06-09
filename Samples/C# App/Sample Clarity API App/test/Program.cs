6e7b0147d77d225007ba55a99fd448c633ca3561
Matthew Huska <mhuska@rjnmail.com>
2022-06-09 16:37:40 -0500

    Finalization and testing of functions.

---
+++ Samples/C# App/Sample Clarity API App/test/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace test
+{
+	static class Program
+	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new Form1());
+
+			var api = new Clarity.Api("", "");
+
+			var projects = api.GetProjects();
+
+			var MyProject = projects.Single((p) => p.projectnumber == "[My Project Number like XX-XXXX-XX]");
+			var sites = MyProject.GetMonitorSites(api);
+		}
+	}
+}
